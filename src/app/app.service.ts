5ba5ea16dd5f66c78dc191807a38c21f15ab9509
name <name@localhost.local>
2020-02-21 16:28:19 +1100

    ss3 clear the way for ng 9

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,7 +3,7 @@ import {throwError as observableThrowError,  Subject ,  Observable } from 'rxjs'
 
 import {catchError, tap, map} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import * as io from 'socket.io-client';
+// import * as io from 'socket.io-client';
 
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
@@ -46,46 +46,7 @@ export class AppService {
     return rightNow - connectedTimestamp;
   }
 
-  addUser(uid){     
-      this.socket.emit('connectUser', uid);
-  }
-
-  getUserByID(uid){
-    return this.users.find(x => x.uID === uid);
-  }
-
-  removeUser(){
-    let observable = new Observable(observer => {
-      this.socket = io(this.url);
-      this.socket.on('disconnectedUser', (data) => {
-        observer.next(data);   
-      });
-      return () => {
-        this.socket.disconnect();
-      }; 
-    })     
-    return observable;
-  }
-
-  reallyRemoveUser(disconnectSocket){
-    let leavingUser = this.users.filter( (user) => {
-        return user.socketID == disconnectSocket;
-      });
-
-      let leavingUserIndex = this.users.indexOf(leavingUser[0]);
-      if(leavingUserIndex > -1){
-        // console.log("LEAVING USER INDEX: ",leavingUserIndex);
-        this.users.splice(leavingUserIndex,1);
-      }
-  }
-
-  kickUser(uid){
-    // console.log("SERVICE ",uid);
-    // get user obj
-    let tempUobj = this.getUserByID(uid);
-
-    this.socket.emit('kickUser', tempUobj);
-  }
+  
   
   sendMessage(message){
     this.socket.emit('newMessage', message);    
@@ -118,40 +79,7 @@ export class AppService {
     return observable;
   }  
 
-  getNewUser() {
-    let observable = new Observable(observer => {
-      this.socket = io(this.url);
-      this.socket.on('newUser', (data) => {
-        observer.next(data);   
-        // console.log('SERVICE NEW USER ',data); 
-        // this.users.push(data);
-      });
-      return () => {
-        // @TODO probs update to @Output()??
-        this.socket.disconnect();
-      }; 
-    })
-
-    return observable;
-  }
-
-  getKickedUser() {
-    let observable = new Observable(observer => {
-      this.socket = io(this.url);
-      this.socket.on('kickedUser', (data) => {
-        observer.next(data);
-      });
-      return () => {
-        this.socket.disconnect();
-      }; 
-    })
-
-    return observable;
-  }
-
-  getUserListLength(){
-    return this.users.length;
-  }
+  
 
   processNewMessage(message){
 
